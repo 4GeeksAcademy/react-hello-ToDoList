d84863b6eb4a9ce748b3927dc328ae8ecfdbf1c2
Giuseppe <132507608+GiuseppeTaylorr@users.noreply.github.com>
2023-08-16 09:40:03 +0000

    toDoList

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
+
 const Home = () => {
+	const [nameList, setNameList] = useState([]);
+
+
+	const handleEnter = async (e) => {
+
+		if (e.keyCode === 13) {
+			let secondName = nameList;
+			secondName.push(e.target.value)
+			setNameList([...secondName])
+		}
+	}
+	const handleDelete = (e) => {
+		let secondName = nameList;
+		secondName.splice(e.target.id, 1)
+		setNameList([...secondName]);
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<div className="row">
+				<div className="col">
+					<div className="card">
+						<div className="card-header">
+							<h1>To Do List</h1>
+						</div>
+						<div className="card-body">
+							<input type="text" className="input" onKeyUp={(e) => handleEnter(e)} placeholder="What´s needs to be done" style={{ color: "black" }} />
+							<ul className="lista">{nameList.map((name, index) => <li key={index}>{name} <button className="button" onClick={(e) => handleDelete(e)} id={index}>X</button></li>)}</ul>
+						</div>
+
+						<footer className="blockquote-footer"> {nameList.length}</footer>
+
+					</div>
+				</div>
+			</div>
 		</div>
-	);
+
+	)
 };
+
+
+
 
 export default Home;
